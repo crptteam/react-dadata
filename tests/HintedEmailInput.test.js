e43745c86a7bf001fe1ca0d822ed197eb62349f2
Alexey Zakharov <embalex@Alexeys-MacBook-Pro.local>
2018-07-05 16:16:26 +0300

    Repaired test settings. Added shallow renderer test.

--- tests/HintedEmailInput.test.js
+++ tests/HintedEmailInput.test.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 
 import { HintedEmailInput } from '../src';
 
 describe('HintedEmailInput', () => {
-  it('Should renders without problems', () => {
+  it('should renders without problems', () => {
     const wrapper = shallow(<HintedEmailInput />);
-
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 });
